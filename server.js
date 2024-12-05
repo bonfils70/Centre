d2e436e07de629485457fc39a416ced660c9f3f8
bonfils70 <bonfilsmbombo@gmail.com>
2024-12-05 15:55:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Route principale
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'paiement.html'));
 });
 
 // Route pour créer une session de paiement
